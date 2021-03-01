2b1a5c70eafee5f8385b6b45c346820237ff24c5
Daniel Rodriguez <daniel.rddev@gmail.com>
2021-03-01 00:19:46 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Nike Store App
+## [Based on https://dribbble.com/shots/14157511-XD-Challenge-003-Scroll](https://dribbble.com/shots/14157511-XD-Challenge-003-Scroll)
---
+++ src/assets/constants/theme.js
@@ -0,0 +1,20 @@
+import { Dimensions } from 'react-native'
+const { width, height } = Dimensions.get('window')
+
+export const COLORS = {
+
+}
+
+export const SIZES = {
+
+  // app dimensions
+  width,
+  height
+}
+
+export const FONTS = {
+
+}
+
+const appTheme = { COLORS, SIZES, FONTS }
+export default appTheme
