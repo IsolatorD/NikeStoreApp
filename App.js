69e28439d148b0128343f47846fa552839c4ea14
Daniel Rodriguez <daniel.rddev@gmail.com>
2021-03-01 02:32:56 -0400

    Finish Design

--- App.js
+++ App.js
@@ -1,114 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React from 'react'
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+  Image,
+  TouchableOpacity
+} from 'react-native'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// Screens
+import { Home } from './src/screens/'
 
-const App: () => React$Node = () => {
+import { icons, COLORS, FONTS, SIZES } from './src/constants/'
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border: 'transparent'
+  }
+}
+
+const Stack = createStackNavigator()
+
+const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        initialRouteName='Home'
+      >
+        <Stack.Screen
+          name='Home'
+          component={Home}
+          options={{
+            title: 'SHOE SELECTOR',
+            headerTintColor: COLORS.lightGray,
+            headerTitleStyle: {
+              ...FONTS.navTitle
+            },
+            headerLeft: ({ onPress }) => (
+              <TouchableOpacity
+                style={{
+                  marginLeft: SIZES.padding
+                }}
+                onPress={onPress}
+              >
+                <Image
+                  source={icons.menu}
+                  resizeMode='contain'
+                  style={{
+                    width: 25,
+                    height: 25
+                  }}
+                />
+              </TouchableOpacity>
+            ),
+            headerRight: ({ onPress }) => (
+              <TouchableOpacity
+                style={{
+                  marginRight: SIZES.padding
+                }}
+                onPress={onPress}
+              >
+                <Image
+                  source={icons.search}
+                  resizeMode='contain'
+                  style={{
+                    width: 30,
+                    height: 30
+                  }}
+                />
+              </TouchableOpacity>
+            )
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+export default () => {
+  return <App />
+}
---
+++ src/screens/Home.js
@@ -0,0 +1,482 @@
+import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  Modal
+} from 'react-native'
+
+import {
+  Svg,
+  Polygon
+} from 'react-native-svg'
+
+import { BlurView } from '@react-native-community/blur'
+
+import { COLORS, DummyData, icons, images, SIZES, FONTS } from '../constants'
+
+const Home = () => {
+  const [showAddtoBagModal, setShowAddToBagModal] = useState(false)
+  const [selectedItem, setSelectedItem] = useState(null)
+  const [selectedSize, setSelectedSize] = useState('')
+  const [trending, setTrending] = useState(DummyData.trendings)
+  const [recentlyViewed, setRecentlyViewed] = useState(DummyData.recentlyViewed)
+
+  const renderTrendingShoes = (item, index) => {
+    let trendingStyle = {}
+
+    if (index == 0) {
+      trendingStyle = { marginLeft: SIZES.padding }
+    }
+
+    return (
+      <TouchableOpacity
+        style={{
+          height: 240,
+          width: 180,
+          justifyContent: 'center',
+          marginHorizontal: SIZES.base,
+          ...trendingStyle
+        }}
+        onPress={() => {
+          setSelectedItem(item)
+          setShowAddToBagModal(true)
+        }}
+      >
+        <Text
+          style={{
+            color: COLORS.gray,
+            ...FONTS.h5
+          }}
+        >
+          {item.type}
+        </Text>
+        <View
+          style={[
+            {
+              flex: 1,
+              justifyContent: 'flex-end',
+              marginTop: SIZES.base,
+              borderRadius: 10,
+              marginRight: SIZES.padding,
+              paddingLeft: SIZES.radius,
+              paddingRight: SIZES.padding,
+              paddingBottom: SIZES.radius,
+              backgroundColor: item.bgColor,
+            },
+            styles.trendingShadow
+          ]}
+        >
+          <View
+            style={{
+              height: '35%',
+              justifyContent: 'space-between'
+            }}
+          >
+            <Text
+              style={{
+                color: COLORS.white,
+                ...FONTS.body4
+              }}
+            >
+              {item.name}
+            </Text>
+            <Text
+              style={{
+                color: COLORS.white,
+                ...FONTS.h3
+              }}
+            >
+              {item.price}
+            </Text>
+          </View>
+        </View>
+        
+        <View
+          style={{
+            position: 'absolute',
+            top: 27,
+            right: 0,
+            width: '95%',
+            height: '100%'
+          }}
+        >
+          <Svg
+            height="100%"
+            width="100%"
+          >
+            <Polygon
+              points="0,0 160,0 160,80"
+              fill="white"
+            />
+          </Svg>
+        </View>
+
+        <Image
+          source={item.img}
+          resizeMode='cover'
+          style={{
+            position: 'absolute',
+            top: 50,
+            right: 0,
+            width: '98%',
+            height: 80,
+            transform: [
+              { rotate: '-15deg' }
+            ]
+          }}
+        />
+      </TouchableOpacity>
+    )
+  }
+
+  const renderRecentlyViewed = (item, index) => {
+    return (
+      <TouchableOpacity
+        key={index}
+        style={{
+          flex: 1,
+          flexDirection: 'row'
+        }}
+        onPress={() => {
+          setSelectedItem(item)
+          setShowAddToBagModal(true)
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <Image
+            source={item.img}
+            resizeMode='contain'
+            style={{
+              width: 130,
+              height: 100
+            }}
+          />
+        </View>
+        <View
+          style={{
+            flex: 1.5,
+            marginLeft: SIZES.radius,
+            justifyContent: 'center'
+          }}
+        >
+          <Text
+            style={{
+              color: COLORS.gray,
+              ...FONTS.body3
+            }}
+          >
+            {item.name}
+          </Text>
+          <Text
+            style={{
+              ...FONTS.h3
+            }}
+          >
+            {item.price}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+  const renderShoeSizes = () => {
+    return (
+      selectedItem.sizes.map((item, index) => {
+        return (
+          <TouchableOpacity
+            key={index}
+            style={{
+              width: 35,
+              height: 25,
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginHorizontal: 5,
+              marginBottom: 10,
+              borderWidth: 1,
+              borderColor: COLORS.white,
+              borderRadius: 5,
+              backgroundColor: selectedItem.sizes[index] == selectedSize ? COLORS.white : null
+            }}
+            onPress={() => setSelectedSize(item)}
+          >
+            <Text
+              style={{
+                color: selectedItem.sizes[index] == selectedSize ? COLORS.black : COLORS.white,
+                ...FONTS.body4
+              }}
+            >
+              {item}
+            </Text>
+          </TouchableOpacity>
+        )
+      })
+    )
+  }
+
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text
+        style={{
+          marginTop: SIZES.radius,
+          marginHorizontal: SIZES.padding,
+          ...FONTS.largeTitleBold
+        }}
+      >
+        TRENDING
+      </Text>
+      {/* Trending */}
+      <View
+        style={{
+          height: 260,
+          marginTop: SIZES.radius
+        }}
+      >
+        <FlatList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={trending}
+          keyExtractor={item => `${item.id}`}
+          renderItem={({ item, index }) => renderTrendingShoes(item, index)}
+        />
+      </View>
+      {/* Recently viewed */}
+      <View
+        style={[
+          {
+            flex: 1,
+            flexDirection: 'row',
+            marginTop: SIZES.padding,
+            borderTopLeftRadius: 30,
+            borderTopRightRadius: 30,
+            backgroundColor: COLORS.white
+          },
+          styles.recentContainerShadow
+        ]}
+      >
+        <View
+          style={{
+            width: 70,
+            marginLeft: SIZES.base
+          }}
+        >
+          <Image
+            source={images.recentlyViewedLabel}
+            resizeMode='contain'
+            style={{
+              width: '100%',
+              height: '100%'
+            }}
+          />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            paddingBottom: SIZES.padding
+          }}
+        >
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            data={recentlyViewed}
+            keyExtractor={item => `${item.id}`}
+            renderItem={({ item, index }) => renderRecentlyViewed(item, index)}
+          />
+        </View>
+      </View>
+      {/* Modal */}
+      {
+        selectedItem &&
+        <Modal
+          animationType='slide'
+          transparent={true}
+          visible={showAddtoBagModal}
+        >
+          <BlurView
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+            blurType='light'
+            blurAmount={20}
+            reducedTransparencyFallbackColor="white"
+          >
+            {/* Button close modal */}
+            <TouchableOpacity
+              style={styles.absolute}
+              onPress={() => {
+                setSelectedItem(null)
+                setSelectedSize('')
+                setShowAddToBagModal(false)
+              }}
+            >
+            </TouchableOpacity>
+            {/* Modal content */}
+            <View
+              style={{
+                justifyContent: 'center',
+                width: '85%',
+                backgroundColor: selectedItem.bgColor
+              }}
+            >
+              <View
+                style={{
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  marginTop: -SIZES.padding * 2
+                }}
+              >
+                <Image
+                  source={selectedItem.img}
+                  resizeMode='contain'
+                  style={{
+                    width: '90%',
+                    height: 170,
+                    transform: [
+                      { rotate: '-15deg' }
+                    ]
+                  }}
+                />
+              </View>
+              <Text
+                style={{
+                  marginTop: SIZES.padding,
+                  marginHorizontal: SIZES.padding,
+                  color: COLORS.white,
+                  ...FONTS.body2
+                }}
+              >
+                {selectedItem.name}
+              </Text>
+              <Text
+                style={{
+                  marginTop: SIZES.base / 2,
+                  marginHorizontal: SIZES.padding,
+                  color: COLORS.white,
+                  ...FONTS.body3
+                }}
+              >
+                {selectedItem.type}
+              </Text>
+              <Text
+                style={{
+                  marginTop: SIZES.radius,
+                  marginHorizontal: SIZES.padding,
+                  color: COLORS.white,
+                  ...FONTS.h1
+                }}
+              >
+                {selectedItem.price}
+              </Text>
+
+              <View
+                style={{
+                  flexDirection: 'row',
+                  marginTop: SIZES.radius,
+                  marginHorizontal: SIZES.padding
+                }}
+              >
+                <View
+                  style={{
+
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: COLORS.white,
+                      ...FONTS.body3
+                    }}
+                  >
+                    Select Size
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flex: 1,
+                    flexWrap: 'wrap',
+                    flexDirection: 'row',
+                    marginLeft: SIZES.radius
+                  }}
+                >
+                  {renderShoeSizes()}
+                </View>
+              </View>
+
+              <TouchableOpacity
+                style={{
+                  width: '100%',
+                  height: 70,
+                  marginTop: SIZES.base,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: 'rgba(0, 0, 0, 0.5)'
+                }}
+                onPress={() => {
+                  setSelectedItem(null)
+                  setSelectedSize('')
+                  setShowAddToBagModal(false)
+                }}
+              >
+                <Text
+                  style={{
+                    color: COLORS.white,
+                    ...FONTS.largeTitleBold
+                  }}
+                >
+                  Add to Bag
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </BlurView>
+        </Modal>
+      }
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white
+  },
+  trendingShadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7
+  },
+  recentContainerShadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 7
+    },
+    shadowOpacity: 0.43,
+    shadowRadius: 9.51,
+    elevation: 15
+  },
+  absolute: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  }
+})
+
+export default Home
